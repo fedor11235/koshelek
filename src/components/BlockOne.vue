12e3dd5957c464939d9c86b7b2bf35a94a536630
fedor11235 <f.avdeev@list.ru>
2022-05-27 08:54:33 +0300

    Made adaptive and correct columns

--- src/components/BlockOne.vue
+++ src/components/BlockOne.vue
@@ -1,48 +1,74 @@
 <template>
-    <div class="block">
-        <p>Hi I'm the first block</p>
-        <div class="table">
-          <div class="col" v-for="(items, index) in crypto" :key="index" >
-            <div class="title">{{index}}</div>
-            <div class="text" v-if="index!=='a' & index!=='b'">{{items}}</div>
-            <div v-if="index==='a' | index==='b'">
-              <div class="text" v-for="(item, index) in items" :key="index">
-                {{item[0]}} : {{item[1]}}
-              </div>
+  <div class="block">
+    {{width}}
+    <p>Hi I'm the first block</p>
+    <div class="table">
+      <div class="col" v-for="(items, index) in crypto" :key="index">
+        <div class="title">{{ index }}</div>
+          <div class="body" >
+            <div class="text" v-for="(item, index) in items" :key="index">
+              <div class="subtitle">{{ index }}</div>
+              <div v-if="index !== 'Total'">{{ item[0] }} : {{ item[1] }}</div>
+              <div v-if="index === 'Total'">{{ item }}</div>
+              <!-- <div class="subtitle" v-show="index !== 'Total'">{{ index }}</div>
+              <div  v-show="index !== 'Total'">{{ item[0] }} : {{ item[1] }}</div>
+
+              <div class="subtitle" v-show="index === 'Total' & width>600">{{ index }}</div>
+              <div  v-show="index === 'Total' & width>600">{{ item }}</div> -->
             </div>
-          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BlockOne',
+  name: "BlockOne",
   data() {
     return {
-      crypto
+      width: 0,
+      crypto,
+    };
+  },
+  methods: {
+    updateWidth() {
+      this.width = window.innerWidth;
+      if(this.width>600){
+        this.crypto.Bid.Total = parseFloat(this.crypto.Bid.Price) + parseFloat(this.crypto.Bid.Amount);
+        this.crypto.Ask.Total = parseFloat(this.crypto.Ask.Price) + parseFloat(this.crypto.Ask.Amount);
+      } else {
+        delete this.crypto.Bid.Total
+        delete this.crypto.Ask.Total
+      }
     }
   },
   created() {
-    this.emitter.on('get-crypto', (event) => {
-      this.crypto = event
-      console.log(event)
-    })
-  }
-}
+    this.width=window.innerWidth
+    window.addEventListener('resize', this.updateWidth);
+    this.emitter.on("get-crypto", (event) => {
+      this.crypto = event;
+      if(this.width>600){
+        this.crypto.Bid.Total = parseFloat(this.crypto.Bid.Price) + parseFloat(this.crypto.Bid.Amount);
+        this.crypto.Ask.Total = parseFloat(this.crypto.Ask.Price) + parseFloat(this.crypto.Ask.Amount);
+      }
+      console.log(this.crypto);
+    });
+  },
+};
 </script>
 
 <style scoped>
 .block {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .table {
-    display: flex;
-    width: 100%;
-    margin: 0 -5px;
-    flex:1;
+  display: flex;
+  width: 100%;
+  margin: 0 -5px;
+  flex: 1;
 }
 .col {
   flex: 1;
@@ -54,7 +80,25 @@ export default {
   color: #f2f2f2;
 }
 
+.body{
+  display: flex;
+  flex: 1;
+}
+
+.subtitle{
+  background-color: #95969e;
+  color: #f2f2f2;
+  flex: 1;
+  padding: 0 1px;
+  text-align: center;
+}
+
 .text {
+  background-color: #575555;
+  color: #f2f2f2;
   font-size: 12px;
+  flex: 1;
+  padding: 0 1px;
+  text-align: center;
 }
 </style>
--- src/plugins/ws.js
+++ src/plugins/ws.js
@@ -2,7 +2,18 @@ import emitter from '@/plugins/emitter.js'
 const ws = new WebSocket("wss://stream.binance.com:9443/ws/bnbbtc@depth")
 ws.onmessage = (event) => {
     const stockObject = JSON.parse(event.data)
-    emitter.emit('get-crypto', stockObject)
+    emitter.emit('get-crypto', {
+        'Bid': {
+            'Price': stockObject.b[0], 
+            'Amount': stockObject.b[1],
+        }, 
+        'Ask': {
+            'Price': stockObject.a[0], 
+            'Amount': stockObject.a[1],   
+        }
+    })
 }
 
 export default ws
+
+//https://api.binance.com/api/v3/depth?symbol=BNBBTC&limit=1000 .
